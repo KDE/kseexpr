6ffdfd08585aebff495d5580ba687b3cd9927267
L. E. Segovia <amy@amyspark.me>
2020-06-02 16:12:56 +0000

    Fix mantissa truncation under non-C locales
    atof(), oft used in Bison parsers, is vulnerable to the current set locale.
    If said locale (like es_ES.utf-8) uses the comma as the decimal point,
    it will never accept the point-separated input that SeExpr receives.

--- src/SeExpr2/Platform.h
+++ src/SeExpr2/Platform.h
@@ -14,6 +14,33 @@
  You may obtain a copy of the License at
  http://www.apache.org/licenses/LICENSE-2.0
 */
+/*
+  The function crack_atof is adapted from the version at
+  https://github.com/shaovoon/floatbench/blob/b13794c63d824b95998dac18b8cf3d05e7d84e57/BenchmarkFloatConv/BenchmarkFloatConv/BenchmarkFloatConv.cpp#L88-L171
+
+  MIT License
+
+  Copyright (c) 2016 shaovoon
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 #ifndef Platform_h
 #define Platform_h
 
@@ -103,6 +130,85 @@ typedef unsigned int uint32_t;
 #else
 typedef off_t FilePos;
 #endif
+
+// Locale-independent atof() - amyspark
+// Prevents SeExpr missing mantissas if LC_ALL is not "C" (like Qt)
+inline double crack_atof(const char* num) {
+  if (!num) {
+    return 0;
+  }
+
+  int sign = 1;
+  double int_part = 0.0;
+  double frac_part = 0.0;
+  bool has_frac = false;
+  bool has_exp = false;
+
+  // +/- sign
+  if (*num == '-') {
+    ++num;
+    sign = -1;
+  } else if (*num == '+') {
+    ++num;
+  }
+
+  while (*num != '\0') {
+    if (*num >= '0' && *num <= '9') {
+      int_part = int_part * 10 + (*num - '0');
+    } else if (*num == '.') {
+      has_frac = true;
+      ++num;
+      break;
+    } else if (*num == 'e') {
+      has_exp = true;
+      ++num;
+      break;
+    } else {
+      return sign * int_part;
+    }
+    ++num;
+  }
+
+  if (has_frac) {
+    double frac_exp = 0.1;
+
+    while (*num != '\0') {
+      if (*num >= '0' && *num <= '9') {
+        frac_part += frac_exp * (*num - '0');
+        frac_exp *= 0.1;
+      } else if (*num == 'e') {
+        has_exp = true;
+        ++num;
+        break;
+      } else {
+        return sign * (int_part + frac_part);
+      }
+      ++num;
+  }
+  }
+
+  // parsing exponent part
+  double exp_part = 1.0;
+  if (*num != '\0' && has_exp) {
+    int exp_sign = 1;
+    if (*num == '-') {
+      exp_sign = -1;
+      ++num;
+    } else if (*num == '+') {
+        ++num;
+    }
+
+    int e = 0;
+    while (*num != '\0' && *num >= '0' && *num <= '9') {
+      e = e * 10 + *num - '0';
+    ++num;
+    }
+
+    exp_part = pow(exp_sign * e, 10);
+  }
+
+  return sign * (int_part + frac_part) * exp_part;
+}
 
 namespace SeExpr2 {
 #if !(defined(WINDOWS) && defined(_MSC_VER))
