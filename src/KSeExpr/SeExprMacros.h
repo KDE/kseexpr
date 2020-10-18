735958fdd438ba76b84caa167c5c4ee14be4f03d
L. E. Segovia <amy@amyspark.me>
2020-10-18 00:48:56 +0000

    Assorted cleanup in KSeExpr

--- src/KSeExpr/SeExprMacros.h
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2011-2019 Disney Enterprises, Inc.
-// SPDX-License-Identifier: LicenseRef-Apache-2.0
-
-#ifndef SeExprMacros_h
-#define SeExprMacros_h
-
-#define UNUSED(x) (void)(x)
-
-#endif
