8939da68c13e16cc5a14b29dabe3145a5b63ab7c
L. E. Segovia <amy@amyspark.me>
2020-10-22 17:09:23 +0000

    Modernize asciiCalculator demo to C++14

--- src/demos/asciiCalculator.cpp
+++ src/demos/asciiCalculator.cpp
@@ -3,11 +3,12 @@
 // SPDX-FileCopyrightText: 2020 L. E. Segovia <amy@amyspark.me>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <KSeExpr/Expression.h>
 #include <KSeExpr/ExprFunc.h>
+#include <KSeExpr/Expression.h>
 #include <KSeExpr/Vec.h>
-#include <cstdlib>
+#include <cassert>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 
 #define STACK_DEPTH 256
@@ -18,36 +19,40 @@ using namespace KSeExpr;
    @file asciiCalculator.cpp
 */
 //! Simple expression class to support our function calculator
-class CalculatorExpr : public Expression {
-  public:
+class CalculatorExpr : public Expression
+{
+public:
     //! Constructor that takes the expression to parse
-    CalculatorExpr(const std::string& expr) : Expression(expr), _count(0) {
+    CalculatorExpr(const std::string &expr)
+        : Expression(expr)
+    {
         for (int i = 0; i < STACK_DEPTH; i++) {
-            stack[i].val = Vec<double, 3, false>(0.0);
-            fail_stack[i] = false;
+            stack.at(i).val = Vec<double, 3, false>(0.0);
+            fail_stack.at(i) = false;
         }
     };
 
     //! Empty constructor
-    CalculatorExpr() : Expression(), _count(0) {
-        for (int i = 0; i < STACK_DEPTH; i++) fail_stack[i] = false;
-    };
+    CalculatorExpr()
+        : Expression() {};
 
     //! Push current result on stack
-    void push() {
+    void push()
+    {
         if (returnType().isString()) {
             evalStr();
         } else if (returnType().isFP()) {
-            const double* val = evalFP();
+            const double *val = evalFP();
             int dim = returnType().dim();
-            for (int k = 0; k < 3; k++) std::cerr << val[k] << " ";
+            for (int k = 0; k < 3; k++)
+                std::cerr << val[k] << " ";
             std::cerr << std::endl;
             if (dim == 1)
-                stack[_count].val = Vec<double, 3, false>(val[0]);
+                stack.at(_count).val = Vec<double, 3, false>(val[0]);
             else if (dim == 2)
-                stack[_count].val = Vec<double, 3, false>(val[0], val[1], 0);
+                stack.at(_count).val = Vec<double, 3, false>(val[0], val[1], 0);
             else if (dim == 3)
-                stack[_count].val = Vec<double, 3, true>(const_cast<double*>(&val[0]));
+                stack.at(_count).val = Vec<double, 3, true>(const_cast<double *>(&val[0]));
             else {
                 std::cerr << "Return type FP(" << dim << ") ignoring" << std::endl;
             }
@@ -57,53 +62,70 @@ class CalculatorExpr : public Expression {
     };
 
     //! Failed attempt; push 0 on stack
-    void fail_push() {
-        fail_stack[_count] = true;
-        stack[_count].val = Vec<double, 3, false>(0.0);
+    void fail_push()
+    {
+        fail_stack.at(_count) = true;
+        stack.at(_count).val = Vec<double, 3, false>(0.0);
         _count++;
     };
 
-    Vec<double, 3, false> peek() { return stack[_count - 1].val; }
+    Vec<double, 3, false> peek()
+    {
+        return stack.at(_count - 1).val;
+    }
 
-    int count() const {
+    int count() const
+    {
         return _count;
     };
 
-  private:
+private:
     //! Simple variable that just returns its internal value
     struct SimpleVar : public ExprVarRef {
-        SimpleVar() : ExprVarRef(ExprType().FP(3).Varying()), val(0.0) {}
-
-        Vec<double, 3, false> val;  // independent variable
-
-        void eval(double* result) {
-            for (int k = 0; k < 3; k++) result[k] = val[k];
+        SimpleVar()
+            : ExprVarRef(ExprType().FP(3).Varying())
+            , val(0.0)
+        {
         }
 
-        void eval(const char** result) {}
+        Vec<double, 3, false> val; // independent variable
+
+        void eval(double *result) override
+        {
+            for (int k = 0; k < 3; k++)
+                result[k] = val[k];
+        }
+
+        void eval(const char **) override
+        {
+            assert(false);
+        }
     };
 
     //! previous computations
-    mutable SimpleVar stack[STACK_DEPTH];
-    mutable bool fail_stack[STACK_DEPTH];
-    mutable int _count;
+    mutable std::array<SimpleVar, STACK_DEPTH> stack;
+    mutable std::array<bool, STACK_DEPTH> fail_stack {};
+    mutable int _count {0};
 
     //! resolve function that only supports one external variable 'x'
-    ExprVarRef* resolveVar(const std::string& name) const {
+    ExprVarRef *resolveVar(const std::string &name) const override
+    {
         if (name[0] == '_') {
-            int position = atoi(name.substr(1, name.size() - 1).c_str());
-            if (position >= count()) std::cerr << "Use of unused result line." << std::endl;
-            if (fail_stack[position]) std::cerr << "Use of invalid result line." << std::endl;
-            return &(stack[position]);
+            int position = std::stoi(name.substr(1, name.size() - 1));
+            if (position >= count())
+                std::cerr << "Use of unused result line." << std::endl;
+            if (fail_stack.at(position))
+                std::cerr << "Use of invalid result line." << std::endl;
+            return &(stack.at(position));
         };
-        addError(ErrorCode::UndeclaredVariable, { name }, 0, 0);
-        return 0;
+        addError(ErrorCode::UndeclaredVariable, {name}, 0, 0);
+        return nullptr;
     };
 };
 
-int main(int argc, char* argv[]) {
-
-    std::cout << "SeExpr Basic Calculator";
+int main(int, char *[])
+{
+    std::cout << "KSeExpr Basic Calculator";
 
     CalculatorExpr expr;
     while (true) {
@@ -117,7 +139,8 @@ int main(int argc, char* argv[]) {
             str = "q";
         };
 
-        if (str == "quit" || str == "q") break;
+        if (str == "quit" || str == "q")
+            break;
         expr.setDesiredReturnType(ExprType().FP(3));
         expr.setExpr(str);
 
