e07d9d1771703e35dde0db61b9a4cadc04f987e4
L. E. Segovia <amy@amyspark.me>
2020-10-22 15:37:05 +0000

    clang-tidy: ExprType

--- src/KSeExpr/ExprType.h
+++ src/KSeExpr/ExprType.h
@@ -3,19 +3,14 @@
 // SPDX-FileCopyrightText: 2020 L. E. Segovia <amy@amyspark.me>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#ifndef ExprType_h
-#define ExprType_h
+#pragma once
 
-#include <vector>
-#include <string>
-#include <map>
 #include <cassert>
 #include <sstream>
+#include <string>
 
-#pragma push_macro("None")
-#undef None
-
-namespace KSeExpr {
+namespace KSeExpr
+{
 /** Describes an allowable type in the SeExpr parse tree
   There are four classes of types in SeExpr: tERROR, tFP, tSTRING, tNONE.
   These can be further modified by the lifetime modifier ltERROR, ltVARYING, ltUNIFORM, ltCONSTANT
@@ -25,73 +20,96 @@ namespace KSeExpr {
   ExprType().FP(3).Uniform() // make uniform
 
  **/
-class ExprType {
-  public:
+class ExprType
+{
+public:
     //! Possible types
     enum Type {
-        tERROR = 0,  ///< Error type (bad things happened here or upstream in tree)
-        tFP,         ///< Floating point type (this combines with _d member to make vectors)
-        tSTRING,     ///< String type
+        tERROR = 0, ///< Error type (bad things happened here or upstream in tree)
+        tFP,        ///< Floating point type (this combines with _d member to make vectors)
+        tSTRING,    ///< String type
         tNONE
-    };  ///< None type (anything like function prototypes or blocks of assignments)
+    }; ///< None type (anything like function prototypes or blocks of assignments)
 
     //! Lifetimes that are possible for type, note the order is from highest to lowest priority for promotion
     enum Lifetime {
-        ltERROR = 0,  ///< Error in lifetime (uniform data depending on varying etc.)
-        ltVARYING,    ///< Varying data (i.e. changes per evaluation point)
-        ltUNIFORM,    ///< Uniform data (i.e. changes only on grids or pixel tiles, depending on how expr used)
-        ltCONSTANT    ///< Constant data (i.e. sub parts of the tree that need only be computed once)
+        ltERROR = 0, ///< Error in lifetime (uniform data depending on varying etc.)
+        ltVARYING,   ///< Varying data (i.e. changes per evaluation point)
+        ltUNIFORM,   ///< Uniform data (i.e. changes only on grids or pixel tiles, depending on how expr used)
+        ltCONSTANT   ///< Constant data (i.e. sub parts of the tree that need only be computed once)
     };
 
     //! Default constructor for a type (error and lifetime error)
-    ExprType() : _type(tERROR), _n(1), _lifetime(ltERROR) {}
+    ExprType() = default;
+
+    //! Default destructor
+    ~ExprType() = default;
 
     //! Fully specified type
-    ExprType(Type type, int n, Lifetime lifetime) : _type(type), _n(n), _lifetime(lifetime) {
+    ExprType(Type type, int n, Lifetime lifetime)
+        : _type(type)
+        , _n(n)
+        , _lifetime(lifetime)
+    {
         assert(_n >= 1);
         assert(_type == tFP || _n == 1);
     }
 
     //! Copy constructor
-    ExprType(const ExprType& other) : _type(other.type()), _n(other.dim()), _lifetime(other.lifetime()) {
+    ExprType(const ExprType &other)
+        : _type(other.type())
+        , _n(other.dim())
+        , _lifetime(other.lifetime())
+    {
         assert(_n >= 1);
         assert(_type == tFP || _n == 1);
     }
 
+    //! Default move constructor
+    ExprType(ExprType &&) = default;
+
     //! Assignment operator
-    ExprType& operator=(const ExprType& other) = default;
+    ExprType &operator=(const ExprType &other) = default;
+    ExprType &operator=(ExprType &&) = default;
 
     /// Returns true if this and other do not match on type and dimension
-    bool operator!=(const ExprType& other) const { return !(*this == other); }
+    bool operator!=(const ExprType &other) const
+    {
+        return !(*this == other);
+    }
 
     /// Returns true if this and other match type and dimension
-    bool operator==(const ExprType& other) const {
+    bool operator==(const ExprType &other) const
+    {
         return (type() == other.type() && dim() == other.dim() && lifetime() == other.lifetime());
     }
 
     ///@{ @name Basic type mutator constructors
 
     /// Mutate this into a none type
-    // TODO: "None" is bad name, /usr/include/X11/X.h potentially text replaces it.
-    ExprType& None() {
+    ExprType &None()
+    {
         _type = tNONE;
         _n = 1;
         return *this;
     }
     /// Mutate this into a floating point type of dimension d
-    ExprType& FP(int d) {
+    ExprType &FP(int d)
+    {
         _type = tFP;
         _n = d;
         return *this;
     }
     /// Mutate this into a string type
-    ExprType& String() {
+    ExprType &String()
+    {
         _type = tSTRING;
         _n = 1;
         return *this;
     }
     /// Mutate this into an error type
-    ExprType& Error() {
+    ExprType &Error()
+    {
         _type = tERROR;
         _n = 1;
         return *this;
@@ -101,22 +119,26 @@ class ExprType {
     ///@{ @name Basic lifetime mutator constructors
 
     /// Mutate this into a constant lifetime
-    ExprType& Constant() {
+    ExprType &Constant()
+    {
         _lifetime = ltCONSTANT;
         return *this;
     }
     /// Mutate this into a uniform lifetime
-    ExprType& Uniform() {
+    ExprType &Uniform()
+    {
         _lifetime = ltUNIFORM;
         return *this;
     }
     /// Mutate this into a varying lifetime
-    ExprType& Varying() {
+    ExprType &Varying()
+    {
         _lifetime = ltVARYING;
         return *this;
     }
     /// Mutate this into a lifetime error
-    ExprType& LifeError() {
+    ExprType &LifeError()
+    {
         _lifetime = ltERROR;
         return *this;
     }
@@ -125,19 +147,22 @@ class ExprType {
     /// @{ @name Lifetime propagation and demotion
 
     //! Assign the lifetime from type a to be my type
-    ExprType& setLifetime(const ExprType& a) {
+    ExprType &setLifetime(const ExprType &a)
+    {
         _lifetime = a.lifetime();
         return *this;
     }
 
     //! Combine the lifetimes (min wins) of a and b and then assign them to this
-    ExprType& setLifetime(const ExprType& a, const ExprType& b) {
+    ExprType &setLifetime(const ExprType &a, const ExprType &b)
+    {
         a.lifetime() < b.lifetime() ? setLifetime(a) : setLifetime(b);
         return *this;
     }
 
     //! Combine the lifetimes (min wins) of a and b and then assign them to this
-    ExprType& setLifetime(const ExprType& a, const ExprType& b, const ExprType& c) {
+    ExprType &setLifetime(const ExprType &a, const ExprType &b, const ExprType &c)
+    {
         setLifetime(a, b);
         setLifetime(*this, c);
         return *this;
@@ -148,23 +173,53 @@ class ExprType {
     /// @{ @name Accessors and predicates
 
     // accessors
-    Type type() const { return _type; }
-    int dim() const { return _n; }
-    Lifetime lifetime() const { return _lifetime; }
+    Type type() const
+    {
+        return _type;
+    }
+    int dim() const
+    {
+        return _n;
+    }
+    Lifetime lifetime() const
+    {
+        return _lifetime;
+    }
 
     //! Direct is predicate checks
-    bool isFP() const { return _type == tFP; }
-    bool isFP(int d) const { return _type == tFP && _n == d; }
-    bool isValue() const { return _type == tFP || _type == tSTRING; }
-    bool isValid() const { return !isError() && !isLifetimeError(); }
-    bool isError() const { return type() == tERROR; }
-    bool isString() const { return type() == tSTRING; }
-    bool isNone() const { return type() == tNONE; }
+    bool isFP() const
+    {
+        return _type == tFP;
+    }
+    bool isFP(int d) const
+    {
+        return _type == tFP && _n == d;
+    }
+    bool isValue() const
+    {
+        return _type == tFP || _type == tSTRING;
+    }
+    bool isValid() const
+    {
+        return !isError() && !isLifetimeError();
+    }
+    bool isError() const
+    {
+        return type() == tERROR;
+    }
+    bool isString() const
+    {
+        return type() == tSTRING;
+    }
+    bool isNone() const
+    {
+        return type() == tNONE;
+    }
 
     //! Checks if value types are compatible.
-    static bool valuesCompatible(const ExprType& a, const ExprType& b) {
-        return (a.isString() && b.isString()) ||
-               (a._type == tFP && b._type == tFP && (a._n == 1 || b._n == 1 || a._n == b._n));
+    static bool valuesCompatible(const ExprType &a, const ExprType &b)
+    {
+        return (a.isString() && b.isString()) || (a._type == tFP && b._type == tFP && (a._n == 1 || b._n == 1 || a._n == b._n));
     }
 
     /// @}
@@ -172,15 +227,31 @@ class ExprType {
     /// validity check: type is not an error
 
     // lifetime matchers
-    bool isLifetimeConstant() const { return lifetime() == ltCONSTANT; }
-    bool isLifetimeUniform() const { return lifetime() == ltUNIFORM; }
-    bool isLifetimeVarying() const { return lifetime() == ltVARYING; }
-    bool isLifetimeError() const { return lifetime() == ltERROR; }
+    bool isLifetimeConstant() const
+    {
+        return lifetime() == ltCONSTANT;
+    }
+    bool isLifetimeUniform() const
+    {
+        return lifetime() == ltUNIFORM;
+    }
+    bool isLifetimeVarying() const
+    {
+        return lifetime() == ltVARYING;
+    }
+    bool isLifetimeError() const
+    {
+        return lifetime() == ltERROR;
+    }
 
-    bool isLifeCompatible(const ExprType& o) const { return o.lifetime() >= lifetime(); }
+    bool isLifeCompatible(const ExprType &o) const
+    {
+        return o.lifetime() >= lifetime();
+    }
 
     //! Stringify the type into a printable string
-    std::string toString() const {
+    std::string toString() const
+    {
         std::stringstream ss;
 
         if (isLifetimeConstant())
@@ -209,20 +280,19 @@ class ExprType {
         return ss.str();
     }
 
-  private:
+private:
     //! Class of type)
-    Type _type;
+    Type _type {tERROR};
     //! Dimension of type _n==1 ignored if _type != FP
-    int _n;
+    int _n {1};
     //! lifetime of type
-    Lifetime _lifetime;
+    Lifetime _lifetime {ltERROR};
 };
 
 /// Quick way to get a vector type i.e. 3 vec is TypeVec(3)
-inline ExprType TypeVec(int n) { return ExprType().FP(n).Varying(); }
+inline ExprType TypeVec(int n)
+{
+    return ExprType().FP(n).Varying();
+}
 
-}  // namespace
-
-#pragma pop_macro("None")
-
-#endif
+} // namespace KSeExpr
